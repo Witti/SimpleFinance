61766bd4c37626e5e3a6948b757fd7d03e46c00e
Daniel Wittberger <daniel@mg-mediaservices.com>
2016-01-01 18:19:55 +0100

    Updated text on landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Welcome</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    Welcome to the alpha-version of SimpleFinance, the simple trackingtool for your financial transactions.
                 </div>
             </div>
         </div>
