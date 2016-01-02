709368ca10c76aa50353729cbb7d7f634408b658
Daniel Wittberger <daniel@mg-mediaservices.com>
2016-01-02 17:42:29 +0100

    category-color implemented in index

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -7,11 +7,26 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">Categories<a class="pull-right" href="{{ url('/category/create') }}" title="Add new account"><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span></a></div>
                     <div class="panel-body">
-                        <ul>
-                            @foreach($categories as $c)
-                                <li><a href="{{ url('/category/edit',['id' => $c->id]) }}">{{ $c->title }}</a></li>
-                            @endforeach
-                        </ul>
+                        <table class="table table-striped table-hover table-responsive">
+                            <thead>
+                                <tr>
+                                    <th>Category</th>
+                                    <th>Color</th>
+                                    <th>&nbsp;</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($categories as $c)
+                                    <tr>
+                                        <td>{{ $c->title }}</td>
+                                        <td><div class="category-color-box" style="background-color: {{ $c->color }};"></div></td>
+                                        <td align="right">
+                                            <a href="{{ url('/category/edit', ['id' => $c->id]) }}" class="text-warning"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a> <a class="text-danger delthis" href="{{ url('/category/delete', ['id' => $c->id]) }}"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
