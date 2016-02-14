3ee929bbd3bdb651fcc0afd49c873ca2c35aa20d
Daniel Wittberger <daniel@mg-mediaservices.com>
2016-02-14 09:40:09 +0100

    account-creation integrated in index

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -4,31 +4,75 @@
         <!-- Main content -->
 <section class="content">
     @include('layouts.partials.alert')
-            <!-- Default box -->
     <div class="box">
         <div class="box-header with-border">
             <h3 class="box-title">Accounts</h3>
-            <div class="box-tools pull-right">
-                <a class="btn btn-box-tool" href="{{ url('/account/create') }}" title="Add new account"><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span></a>
-            </div>
         </div>
         <div class="box-body">
-            <table id="accountstable" class="table table-bordered table-hover">
-                <thead>
-                    <tr>
-                        <th>Accountname</th>
-                        <th>Current balance</th>
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach($accounts as $a)
-                    <tr>
-                        <td><a href="{{ url('/account/edit',['id' => $a->id]) }}">{{ $a->title }}</a></td>
-                        <td>{{ $a->currentBalance }}</td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
+            <div class="panel-body">
+                <form class="form-horizontal" role="form" method="POST" action="{{ url('/account/store') }}">
+                    {!! csrf_field() !!}
+                    <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+                        <label class="col-md-4 control-label">Title</label>
+
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+
+                            @if ($errors->has('title'))
+                                <span class="help-block">
+                                <strong>{{ $errors->first('title') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group{{ $errors->has('startbalance') ? ' has-error' : '' }}">
+                        <label class="col-md-4 control-label">Startbalance</label>
+
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" name="startbalance" value="{{ old('startbalance') }}">
+
+                            @if ($errors->has('title'))
+                                <span class="help-block">
+                                <strong>{{ $errors->first('startbalance') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            <button type="submit" class="btn btn-primary">
+                                Create
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div><!-- /.box-body -->
+    </div><!-- /.box -->
+
+
+    <div class="box">
+        <div class="box-body">
+            <div class="panel-body">
+                <table id="accountstable" class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>Accountname</th>
+                            <th>Current balance</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($accounts as $a)
+                        <tr>
+                            <td><a href="{{ url('/account/edit',['id' => $a->id]) }}">{{ $a->title }}</a></td>
+                            <td>{{ $a->currentBalance }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div><!-- /.box-body -->
     </div><!-- /.box -->
 </section><!-- /.content -->
