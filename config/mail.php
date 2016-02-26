f25e18720e215f19f7763a6f9d4c53ec64a479a7
Daniel Wittberger <daniel@mg-mediaservices.com>
2016-02-26 11:47:00 +0100

    Mailconfig-Fix

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'simplefinance@wittberger.net', 'name' => 'SimpleFinance Alpha'],
 
     /*
     |--------------------------------------------------------------------------
