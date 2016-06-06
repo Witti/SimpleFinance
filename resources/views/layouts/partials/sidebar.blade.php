8ccec464c91dd24c7dd51f04dd3e1f9fc3041948
Daniel Wittberger <daniel@mg-mediaservices.com>
2016-06-06 22:32:49 +0200

    Removed Userpanel from sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -4,14 +4,14 @@
     <section class="sidebar">
         @if(!Auth::guest())
         <!-- Sidebar user panel -->
-        <div class="user-panel">
+        <!--<div class="user-panel">
             <div class="pull-left image">
                 <img src="{{ gravatar(Auth::user()->email) }}" class="img-circle" alt="User Image">
             </div>
             <div class="pull-left info">
                 <p>{{ Auth::user()->name }}</p>
             </div>
-        </div>
+        </div>-->
         <!-- search form -->
         <!--<form action="#" method="get" class="sidebar-form">
             <div class="input-group">
